bced41d574ff00cf411fb6dd67d462e4114a3c1e
Anderson Lima <anderson.lm30@gmail.com>
2020-08-15 10:39:31 -0300

    Initial Commit

---
+++ src/test/java/com/al121/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.al121.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
